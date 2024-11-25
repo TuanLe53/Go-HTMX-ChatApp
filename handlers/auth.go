29b69099e92e956437392b7e8a949a06f186e587
Tuan Le <leanhtuan030523@gmail.com>
2024-11-25 18:41:51 +0700

    Add HandleSignUpUser endpoint with hx-redirect behavior for testing purposes

--- handlers/auth.go
+++ handlers/auth.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"net/http"
+
 	"github.com/TuanLe53/Go-HTMX-ChatApp/templates"
 	"github.com/labstack/echo/v4"
 )
@@ -13,4 +15,9 @@ func (h AuthHandler) LoginPage(c echo.Context) error {
 
 func (h AuthHandler) SignUpPage(c echo.Context) error {
 	return Render(c, templates.SignUp())
+}
+
+func (h AuthHandler) HandleSignUpUser(c echo.Context) error {
+	c.Response().Header().Set("hx-redirect", "/login")
+	return c.String(http.StatusOK, "Success")
 }
