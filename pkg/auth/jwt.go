a6773f27e6b2fddccf6b0e80912bae3b1fa4f61a
Tuan Le <leanhtuan030523@gmail.com>
2024-11-26 19:44:59 +0700

    Add hashPw, checkPw function, createJWTClaims, GenerateToken

---
+++ pkg/auth/jwt.go
@@ -0,0 +1,37 @@
+package auth
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+)
+
+type jwtClaims struct {
+	ID   string `json:"id"`
+	Name string `json:"name"`
+	jwt.RegisteredClaims
+}
+
+func CreateJWTClaims(id string, name string) *jwtClaims {
+	return &jwtClaims{
+		id,
+		name,
+		jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(time.Now().Add(time.Hour * 24)),
+		},
+	}
+}
+
+func GenerateToken(claims jwtClaims) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+
+	secret := os.Getenv("SECRET_KEY")
+
+	t, err := token.SignedString(secret)
+	if err != nil {
+		return "", err
+	}
+
+	return t, nil
+}
