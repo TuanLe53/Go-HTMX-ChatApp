19816b6df52009d477b83bbf664f2a003c2a1c01
Tuan Le <leanhtuan030523@gmail.com>
2024-11-26 20:01:42 +0700

    Add HandleLoginUser handler

--- handlers/auth.go
+++ handlers/auth.go
@@ -52,3 +52,32 @@ func (h AuthHandler) HandleSignUpUser(c echo.Context) error {
 	c.Response().Header().Set("hx-redirect", "/login")
 	return c.NoContent(http.StatusSeeOther)
 }
+
+func (h AuthHandler) HandleLoginUser(c echo.Context) error {
+	email := c.FormValue("email")
+	password := c.FormValue("password")
+
+	if !IsValidEmail(email) {
+		return Render(c, components.ErrorMessage("Invalid email"))
+	}
+
+	// Check if the email exists
+	user, err := models.FindUserWithEmail(email)
+	if err != nil {
+		return Render(c, components.ErrorMessage(err.Error()))
+	}
+
+	// Check if user is nil (no such user)
+	if user == nil {
+		return Render(c, components.ErrorMessage("User with this email does not exist"))
+	}
+
+	hashedPW := user.Password
+	err = auth.CheckPw(hashedPW, password)
+	if err != nil {
+		return Render(c, components.ErrorMessage("Incorrect password"))
+	}
+
+	c.Response().Header().Set("hx-redirect", "/")
+	return c.NoContent(http.StatusSeeOther)
+}
