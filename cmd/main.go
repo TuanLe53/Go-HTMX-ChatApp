312d3ec7a7f491dab944ede2ecdbd53480e9f447
Tuan Le <leanhtuan030523@gmail.com>
2024-11-26 20:01:01 +0700

    Add route to index page, route to login request

--- cmd/main.go
+++ cmd/main.go
@@ -6,6 +6,7 @@ import (
 	"github.com/TuanLe53/Go-HTMX-ChatApp/db"
 	"github.com/TuanLe53/Go-HTMX-ChatApp/db/models"
 	"github.com/TuanLe53/Go-HTMX-ChatApp/handlers"
+	"github.com/TuanLe53/Go-HTMX-ChatApp/templates"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
@@ -26,8 +27,13 @@ func main() {
 	db := db.DB()
 	db.AutoMigrate(&models.User{})
 
+	app.GET("/", func(c echo.Context) error {
+		return handlers.Render(c, templates.Index())
+	})
+
 	authHandler := handlers.AuthHandler{}
 	app.GET("/login", authHandler.LoginPage)
+	app.POST("/login", authHandler.HandleLoginUser)
 	app.GET("/signup", authHandler.SignUpPage)
 	app.POST("/signup", authHandler.HandleSignUpUser)
 
