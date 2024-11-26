8a554974a1311d7e4c1b565da86955073892920b
Tuan Le <leanhtuan030523@gmail.com>
2024-11-26 19:42:57 +0700

    Add logic to check is email valid, is email already exists, hash password, and create user

--- handlers/auth.go
+++ handlers/auth.go
@@ -2,8 +2,12 @@ package handlers
 
 import (
 	"net/http"
+	"strings"
 
+	"github.com/TuanLe53/Go-HTMX-ChatApp/db/models"
+	"github.com/TuanLe53/Go-HTMX-ChatApp/pkg/auth"
 	"github.com/TuanLe53/Go-HTMX-ChatApp/templates"
+	"github.com/TuanLe53/Go-HTMX-ChatApp/templates/components"
 	"github.com/labstack/echo/v4"
 )
 
@@ -18,6 +22,33 @@ func (h AuthHandler) SignUpPage(c echo.Context) error {
 }
 
 func (h AuthHandler) HandleSignUpUser(c echo.Context) error {
+
+	email := c.FormValue("email")
+	username := c.FormValue("username")
+	password := c.FormValue("password")
+	confirm_password := c.FormValue("confirm_password")
+
+	if !IsValidEmail(email) {
+		return Render(c, components.ErrorMessage("Invalid email"))
+	}
+
+	// Check if the email exists
+	_, err := models.FindUserWithEmail(email)
+	if err != nil {
+		return Render(c, components.ErrorMessage(err.Error()))
+	}
+
+	if strings.TrimSpace(password) != strings.TrimSpace(confirm_password) {
+		return Render(c, components.ErrorMessage("Passwords don't match."))
+	}
+
+	hashedPW, err := auth.HashPw(password)
+	if err != nil {
+		return Render(c, components.ErrorMessage(err.Error()))
+	}
+
+	models.CreateUser(email, username, hashedPW)
+
 	c.Response().Header().Set("hx-redirect", "/login")
-	return c.String(http.StatusOK, "Success")
+	return c.NoContent(http.StatusSeeOther)
 }
