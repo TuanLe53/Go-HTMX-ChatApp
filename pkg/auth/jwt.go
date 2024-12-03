951dab10b7fc696fdf01e8d385b93fce9d5ff457
Tuan Le <leanhtuan030523@gmail.com>
2024-12-03 19:22:44 +0700

    Create ValidateToken func

--- pkg/auth/jwt.go
+++ pkg/auth/jwt.go
@@ -1,20 +1,21 @@
 package auth
 
 import (
+	"fmt"
 	"os"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
 )
 
-type jwtClaims struct {
+type JWTClaims struct {
 	ID   string `json:"id"`
 	Name string `json:"name"`
 	jwt.RegisteredClaims
 }
 
-func CreateJWTClaims(id string, name string) *jwtClaims {
-	return &jwtClaims{
+func CreateJWTClaims(id string, name string) *JWTClaims {
+	return &JWTClaims{
 		id,
 		name,
 		jwt.RegisteredClaims{
@@ -23,7 +24,7 @@ func CreateJWTClaims(id string, name string) *jwtClaims {
 	}
 }
 
-func GenerateToken(claims jwtClaims) (string, error) {
+func GenerateToken(claims *JWTClaims) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 
 	secret := os.Getenv("SECRET_KEY")
@@ -34,4 +35,25 @@ func GenerateToken(claims jwtClaims) (string, error) {
 	}
 
 	return t, nil
+}
+
+func ValidateToken(tokenString string) (*jwt.Token, error) {
+	secret := os.Getenv("SECRET_KEY")
+
+	token, err := jwt.Parse(tokenString, func(t *jwt.Token) (interface{}, error) {
+		if _, ok := t.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", t.Header["alg"])
+		}
+		return []byte(secret), nil
+	})
+
+	if err != nil {
+		return nil, err
+	}
+
+	if !token.Valid {
+		return nil, fmt.Errorf("invalid token")
+	}
+
+	return token, nil
 }
