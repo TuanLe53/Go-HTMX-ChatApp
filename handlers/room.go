25febc70d3a53b57a5743885746121b19d644373
Tuan Le <leanhtuan030523@gmail.com>
2024-12-04 19:05:05 +0700

    Add simple JoinRoom func

--- handlers/room.go
+++ handlers/room.go
@@ -1,6 +1,8 @@
 package handlers
 
 import (
+	"github.com/TuanLe53/Go-HTMX-ChatApp/db/models"
+	"github.com/TuanLe53/Go-HTMX-ChatApp/templates"
 	"github.com/TuanLe53/Go-HTMX-ChatApp/templates/components"
 	"github.com/labstack/echo/v4"
 )
@@ -13,4 +15,12 @@ func (h RoomHandler) GetCreateRoomComponent(c echo.Context) error {
 
 func (h RoomHandler) GetRoomList(c echo.Context) error {
 	return Render(c, components.RoomList())
+}
+
+func (h RoomHandler) JoinRoom(c echo.Context) error {
+	roomID := c.Param("roomID")
+
+	room := models.FindRoomByID(roomID)
+
+	return Render(c, templates.Room(room))
 }
