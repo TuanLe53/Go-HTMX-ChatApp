3cb9efb44471a345aa04f92fbe4b599381668a4d
Tuan Le <leanhtuan030523@gmail.com>
2024-12-07 19:00:07 +0700

    Initialize and start a WebSocket server in the application. Extend database migration to include the  model. Update the root route () to fetch and render a list of rooms. - Add  to manage room-related functionalities:   - Fetch a paginated list of rooms ().   - Add the capability to create new rooms (). Organize routes and ensure proper grouping under  for authenticated access.  Add error handling for fetching room data on the root route. Improve middleware usage for secure endpoints.

--- cmd/main.go
+++ cmd/main.go
@@ -2,11 +2,13 @@ package main
 
 import (
 	"log"
+	"net/http"
 
 	"github.com/TuanLe53/Go-HTMX-ChatApp/db"
 	"github.com/TuanLe53/Go-HTMX-ChatApp/db/models"
 	"github.com/TuanLe53/Go-HTMX-ChatApp/handlers"
 	custommiddleware "github.com/TuanLe53/Go-HTMX-ChatApp/middleware"
+	"github.com/TuanLe53/Go-HTMX-ChatApp/pkg/websocket"
 	"github.com/TuanLe53/Go-HTMX-ChatApp/templates"
 	"github.com/joho/godotenv"
 	"github.com/labstack/echo/v4"
@@ -28,24 +30,38 @@ func main() {
 
 	db.InitDB()
 	db := db.DB()
-	db.AutoMigrate(&models.User{})
+	db.AutoMigrate(&models.User{}, &models.Room{})
+
+	wsServer := websocket.NewWsServer()
+	go wsServer.Start()
 
 	app.GET("/", func(c echo.Context) error {
-		return handlers.Render(c, templates.Index())
+		rooms, err := models.GetRooms(10, 0)
+		if err != nil {
+			log.Println("Error getting room list", err)
+			c.Response().Header().Set("hx-redirect", "/")
+			return c.NoContent(http.StatusSeeOther)
+		}
+		return handlers.Render(c, templates.Index(rooms))
 	})
 
 	authHandler := handlers.AuthHandler{}
+	roomHandler := handlers.RoomHandler{}
+
 	app.GET("/login", authHandler.LoginPage)
 	app.POST("/login", authHandler.HandleLoginUser)
 	app.GET("/signup", authHandler.SignUpPage)
 	app.POST("/signup", authHandler.HandleSignUpUser)
 
+	app.GET("/room/list", roomHandler.GetRoomList)
+
 	authGroup := app.Group("")
 	authGroup.Use(custommiddleware.JWTAuthMiddleware)
 
-	roomHandler := handlers.RoomHandler{}
 	authGroup.GET("/room/new", roomHandler.GetCreateRoomComponent)
-	authGroup.GET("/room/list", roomHandler.GetRoomList)
+	authGroup.POST("/room/new", roomHandler.CreateRoom)
+	// authGroup.GET("/room/:roomID", roomHandler.)
+	// authGroup.GET("/room/join/:roomID", roomHandler.JoinRoom)
 
 	app.Logger.Fatal(app.Start(":5050"))
 }
