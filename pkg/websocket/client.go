78d1338c39e63eadb0bf5092d6ff3319086eb602
Tuan Le <leanhtuan030523@gmail.com>
2024-12-12 18:53:14 +0700

    feat: enhance WebSocket client functionality with server integration
    - Added WsServer and Room fields to the Client struct for better context management.
    - Modified newClient to initialize the new fields.
    - Implemented the Read method for the Client struct:
      - Reads incoming messages from the WebSocket connection.
      - Unmarshals JSON messages into Message structs.
      - Broadcasts messages to the WebSocket server using the Broadcast channel.
      - Handles client unregistration and connection cleanup on errors.
    
    These changes improve the WebSocket server's ability to manage and broadcast messages across rooms.

--- pkg/websocket/client.go
+++ pkg/websocket/client.go
@@ -1,13 +1,44 @@
 package websocket
 
-import "github.com/gorilla/websocket"
+import (
+	"encoding/json"
+	"log"
+
+	"github.com/gorilla/websocket"
+)
 
 type Client struct {
-	conn *websocket.Conn
+	conn     *websocket.Conn
+	WsServer *WSServer
+	Room     *ChatRoom
 }
 
-func newClient(conn *websocket.Conn) *Client {
+func newClient(conn *websocket.Conn, WsServer *WSServer, room *ChatRoom) *Client {
 	return &Client{
-		conn: conn,
+		conn:     conn,
+		WsServer: WsServer,
+		Room:     room,
+	}
+}
+
+func (c *Client) Read() {
+	defer func() {
+		c.WsServer.Unregister <- c
+		c.conn.Close()
+	}()
+
+	for {
+		_, msg, err := c.conn.ReadMessage()
+		if err != nil {
+			log.Println("Read error", err)
+			break
+		}
+
+		var message Message
+		if err := json.Unmarshal(msg, &message); err != nil {
+			log.Fatal("Error Unmarshalling message")
+		}
+
+		c.WsServer.Broadcast <- &message
 	}
 }
