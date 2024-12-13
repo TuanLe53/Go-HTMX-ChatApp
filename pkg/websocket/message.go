c04572fd7e09c607b8bf8d272603ee0f13fec1fb
Tuan Le <leanhtuan030523@gmail.com>
2024-12-13 19:19:27 +0700

    refactor: update Message struct to use UUID for Target
    Replaced the Target field in the Message struct with a uuid.UUID type for improved uniqueness and reliability.

--- pkg/websocket/message.go
+++ pkg/websocket/message.go
@@ -1,7 +1,9 @@
 package websocket
 
+import "github.com/google/uuid"
+
 type Message struct {
-	Sender  *Client `json:"sender"`
-	Message string  `json:"message"`
-	Target  string  `json:"target"`
+	Sender  *Client   `json:"sender"`
+	Message string    `json:"message"`
+	Target  uuid.UUID `json:"target"`
 }
