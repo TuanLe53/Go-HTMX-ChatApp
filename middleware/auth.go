2ec6b16bfb60fab0ae42a6bcb07be4b6f7649b5d
Tuan Le <leanhtuan030523@gmail.com>
2024-12-17 19:34:55 +0700

    Refactor JWT Authentication Middleware
    - Improved error handling when accessing the JWT token cookie.
    - Added logging for invalid token claims and better management of token validation errors.
    - Replaced old token validation logic with updated structure.
    - Set decoded JWT claims in the context for downstream use."

--- middleware/auth.go
+++ middleware/auth.go
@@ -7,12 +7,12 @@ import (
 	"github.com/TuanLe53/Go-HTMX-ChatApp/handlers"
 	"github.com/TuanLe53/Go-HTMX-ChatApp/pkg/auth"
 	"github.com/TuanLe53/Go-HTMX-ChatApp/templates/components"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/labstack/echo/v4"
 )
 
 func JWTAuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 	return func(c echo.Context) error {
-		log.Println("MIDDLEWARE CALL")
 		cookie, err := c.Cookie("access")
 		if err != nil {
 			if err == http.ErrNoCookie {
@@ -20,20 +20,26 @@ func JWTAuthMiddleware(next echo.HandlerFunc) echo.HandlerFunc {
 				log.Println("No access cookie found")
 				return handlers.Render(c, components.AccessDenied())
 			} else {
-				// Some other error
+				// other errors
 				log.Println("Error retrieving cookie:", err)
 				return handlers.Render(c, components.ErrorMessage("Something went wrong. Please try again later"))
 			}
 		}
 
-		// Log the cookie value
-		log.Printf("Token available")
-		_, err = auth.ValidateToken(cookie.Value)
+		token, err := auth.ValidateToken(cookie.Value)
 		if err != nil {
 			log.Println("Invalid token: ", err)
 			c.Response().Header().Set("hx-redirect", "/login")
 			return c.NoContent(http.StatusSeeOther)
 		}
+
+		claims, ok := token.Claims.(jwt.MapClaims)
+		if !ok {
+			log.Println("Invalid token claims: ")
+			c.Response().Header().Set("hx-redirect", "/login")
+			return c.NoContent(http.StatusSeeOther)
+		}
+		c.Set("user", claims)
 
 		return next(c)
 	}
