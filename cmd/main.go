0e91414ffcdfb39a257a4be919c5be4c70a34278
Tuan Le <leanhtuan030523@gmail.com>
2024-12-17 19:29:39 +0700

    Move WebSocket route under authentication group
    - Moved the WebSocket route ("/ws") from the main application to the "authGroup" for better organization and access control.
    - Retained the WebSocket server initialization and usage with "websocket.ServeWS".
    - Ensured that WebSocket access is now restricted under the authentication context.

--- cmd/main.go
+++ cmd/main.go
@@ -34,9 +34,6 @@ func main() {
 
 	wsServer := websocket.NewWsServer()
 	go wsServer.Start()
-	app.GET("/ws", func(c echo.Context) error {
-		return websocket.ServeWS(c, wsServer)
-	})
 
 	app.GET("/", func(c echo.Context) error {
 		rooms, err := models.GetRooms(10, 0)
@@ -70,6 +67,10 @@ func main() {
 	})
 	authGroup.POST("/room/:roomID/verify-password", func(c echo.Context) error {
 		return roomHandler.VerifyPassword(c, wsServer)
+	})
+
+	authGroup.GET("/ws", func(c echo.Context) error {
+		return websocket.ServeWS(c, wsServer)
 	})
 
 	app.Logger.Fatal(app.Start(":5050"))
