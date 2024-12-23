a74e6355449c2808dcf25d1c478c665127e73e5e
Tuan Le <leanhtuan030523@gmail.com>
2024-12-23 14:53:53 +0700

    Add IsSelf to align message

--- pkg/websocket/message.go
+++ pkg/websocket/message.go
@@ -4,6 +4,7 @@ import "github.com/google/uuid"
 
 type Message struct {
 	Sender  *Client   `json:"sender"`
+	IsSelf  bool      `json:"is_self"`
 	Message string    `json:"message"`
 	Target  uuid.UUID `json:"target"`
 	SendAt  string    `json:"send_at"`
